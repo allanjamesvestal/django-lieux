e6aa1147056a702ed6ac9031d0e91c2459481c72
Allan James Vestal <allanjamesvestal@gmail.com>
2013-12-13 14:26:21 -0600

    ANOTHER TYPO SMOTED.

--- lieux/formats.py
+++ lieux/formats.py
@@ -148,7 +148,7 @@ def format_result_in_ap_style(address, db_alias=None, street_custom_styles=None,
                             result_list[6].lower()
                         ].keys():
                 formatted_first_line.append(
-                        street_custom_styles.STREETS_TO_STYLE[
+                        street_custom_styles[
                             result_list[6].lower()
                         ][result_list[2].strip('"').lower()])
         else:
